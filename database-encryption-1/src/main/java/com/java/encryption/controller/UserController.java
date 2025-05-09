632756f339a2065ea593eb1022e63b8d4fc7e312
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-09 15:52:05 +0530

    [column definition] updated
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

--- database-encryption-1/src/main/java/com/java/encryption/controller/UserController.java
+++ database-encryption-1/src/main/java/com/java/encryption/controller/UserController.java
@@ -2,7 +2,6 @@ package com.java.encryption.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,11 +9,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.java.encryption.entity.UserDetails;
 import com.java.encryption.model.UserDetailsModel;
 import com.java.encryption.service.UserDetailsService;
-
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,23 +19,23 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UserController {
 	private final UserDetailsService userDetailsService;
-	
+
 	@PostMapping(path = "/add")
 	public ResponseEntity<String> saveUserDetails(@RequestBody UserDetailsModel userDetailsModel){
-		
+
 		userDetailsService.saveUserDetails(userDetailsModel);
-		
+
 		return ResponseEntity
 								.status(HttpStatus.CREATED)
 								.body("Created User Successfully!");
 	}
-	
+
 	@GetMapping(path = "/get-user-list")
 	public ResponseEntity<List<UserDetailsModel>> getUsersList(){
-		
+
 		List<UserDetails> userList = userDetailsService.getUserDetailsList();
 		List<UserDetailsModel> usersDetailsModelList = new ArrayList<>();
-		
+
 		for(UserDetails element : userList) {
 			UserDetailsModel userDetailsModel = 
 					UserDetailsModel.builder()
@@ -48,10 +45,10 @@ public class UserController {
 					.address(element.getAddress())
 					.city(element.getCity())
 					.build();
-			
+
 			usersDetailsModelList.add(userDetailsModel);
 		}
-		
+
 		return ResponseEntity.status(HttpStatus.OK).body(usersDetailsModelList);
 	}
 
