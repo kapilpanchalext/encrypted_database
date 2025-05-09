5b01d2b9972f5bff5a96aebc6661c16dd1331363
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-09 12:59:14 +0530

    [init] initialized project
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ database-encryption-1/src/main/java/com/java/encryption/DatabaseEncryption1Application.java
@@ -0,0 +1,13 @@
+package com.java.encryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseEncryption1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseEncryption1Application.class, args);
+	}
+
+}
---
+++ database-encryption-1/src/test/java/com/java/encryption/DatabaseEncryption1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.encryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseEncryption1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
