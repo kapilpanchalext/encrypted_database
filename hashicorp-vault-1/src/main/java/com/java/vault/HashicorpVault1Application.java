a0b65040413d07d0bdccd02a11be1e238c166d6a
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-16 09:48:12 +0530

    [init] hashicorp vault
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ hashicorp-vault-1/src/main/java/com/java/vault/HashicorpVault1Application.java
@@ -0,0 +1,13 @@
+package com.java.vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HashicorpVault1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HashicorpVault1Application.class, args);
+	}
+
+}
