6991bf54ab2f4248225354909ff5486b623e1fbf
kapil.panchal.ext@nmims.edu <kapil.panchal.ext@nmims.edu>
2025-05-16 10:56:07 +0530

    [docker] compose file
    Code Quality:
    - formatted -   [ ]
    - mvn clean -   [ ]
    - mvn install - [ ]
    
    Related ticket(s):

---
+++ hashicorp-vault-1/src/test/java/com/java/vault/HashicorpVault1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HashicorpVault1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
